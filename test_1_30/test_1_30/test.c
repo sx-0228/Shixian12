080f6bbdeb7d09f834a9e0a3084ad8e3a8899d61
sx-0228 <1478280248@qq.com>
2021-02-01 16:39:01 +0800

    提交代码

---
+++ test_1_30/test_1_30/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+
+//int main()
+//{
+//	//printf("c:\\test\\32\\test");
+//	//printf("%c\n", '\'');
+//	printf("%s\n", "\"");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("abc\n");
+//	return 0;
+//}
