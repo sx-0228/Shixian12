e9550d157d4d7adeb96211822feebfae4f0bd2b6
sx-0228 <1478280248@qq.com>
2021-02-09 18:15:44 +0800

    提交代码

---
+++ test_2_9/test_2_9/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include<string.h>
+//
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？(1/0)\n");
+//	scanf("%d", &input);//1/0
+//	if (input = 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//
+//	return 0;
+//}
+
+int main()
+{
+	int line = 0;
+	printf("加入比特\n")；
+
+    while(line<20000)
+	{ 
+	printf("敲一行代码\n");
+
+
+	return 0;
+
+}
